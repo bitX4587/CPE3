7c6145365b20de8f22536c8f9e9ff00b09bbc218
bitX4587 <158537348+bitX4587@users.noreply.github.com>
2024-03-20 07:31:26 +0800

    polymorphism of animals (WIP).py

--- POLYMORPH/polymorphism of animals (WIP).py
+++ POLYMORPH/polymorphism of animals (WIP).py
@@ -1,4 +1,6 @@
-#Parent Class
+def clear_screen():
+    print('\n' * 100)  # Adjust the number based on your console size
+# Parent Class
 class Animal:
     def __init__(self) -> None:
         pass
@@ -9,164 +11,267 @@ class Animal:
     def printAnimals(self):
         print("\n****************************************")
         print("Choices:")
-        print("\t1) Mammal")
-        print("\t2) Amphibian")
-        print("\t3) Fish")
-        print("\t4) Birds")
-        print("\t5) Reptiles")
-        print("\t6) Exit")
+        print("\tMammal")
+        print("\tAmphibian")
+        print("\tFish")
+        print("\tBird")
+        print("\tReptile")
+        print("\tExit")
         print("\n****************************************")
 
-#Child Class Mammal
+
+# Child Class Mammal
 class Mammal(Animal):
     def __init__(self) -> None:
         pass
+
     def show(self):
         print("""
               \tMammal:
 
                 A warm-blooded vertebrate animal of a class 
                 that is distinguished by the possession of hair
-                or fur,the secretion of milk by females for the
+                or fur, the secretion of milk by females for the
                 nourishment of the young, and (typically) the 
                 birth of live young.
               """)
+
     def alive(self):
         print("\nThis animal is alive!")
+
 
 class Dogs(Mammal):
     def __init__(self) -> None:
         pass
+
     def act(self):
         print("\nthe dog is eating!")
         print("\nWOOF! WOOF! WOOF!")
 
+
 class Cats(Mammal):
     def __init__(self) -> None:
         pass
+
     def act(self):
         print("\nthe cat is eating!")
         print("\nMEOW! MEOW! MEOW!")
-        
-#Child Class Amphibian
+
+
+# Child Class Amphibian
 class Amphibian(Animal):
     def __init__(self) -> None:
         pass
+
     def show(self):
         print("""
               \tAmphibian:
-              
+
                 A cold-blooded vertebrate animal of a class 
                 that comprises the frogs, toads, newts, and 
                 salamanders. They are distinguished by having 
                 an aquatic gill-breathing larval stage followed 
                 (typically) by a terrestrial lung-breathing adult stage.
               """)
+
     def alive(self):
         print("\nThis animal is alive!")
-        
+
+
 class Frogs(Amphibian):
     def __init__(self) -> None:
         pass
+
     def act(self):
         print("\nthe frog is hopping!")
         print("\nKOKAK! KOKAK! KOKAK!")
 
-class Snakes(Amphibian):
+
+class Salamander(Amphibian):
     def __init__(self) -> None:
         pass
-    def act(self):
-        print("\nthe snake is crawling!")
-        print("\nHISS! HISS! HISS!")
 
-#Child Class Fish
+    def act(self):
+        print("\nthe Salamander is crawling!")
+        print("\nFRIK! FRIK! FRIK!")
+
+
+# Child Class Fish
 class Fish(Animal):
     def __init__(self) -> None:
         pass
+
     def show(self):
         print("""
               \tFish:
-              
+
                 A limbless cold-blooded vertebrate animal with gills
                 and fins and living wholly in water.
               """)
+
     def alive(self):
         print("\nThis animal is alive!")
-        
+
+
 class AtlanticBlueMarlin(Fish):
     def __init__(self) -> None:
         pass
+
     def act(self):
         print("\nthe Atlantic Blue Marlin is eating small fishes!")
         print("\nGLUB! GLUB! GLUB!")
 
+
 class Garfish(Fish):
     def __init__(self) -> None:
         pass
+
     def act(self):
         print("\nthe Garfish is breathing in the water!")
         print("\nGULP! GULP! GULP!")
 
-#Child Class Birds
+
+# Child Class Birds
 class Birds(Animal):
     def __init__(self) -> None:
         pass
+
     def show(self):
         print("""
               \tBirds:
-              
+
                 A warm-blooded egg-laying vertebrate distinguished 
                 by the possession of feathers, wings, and a beak and 
                 (typically) by being able to fly.
               """)
+
     def alive(self):
         print("\nThis animal is alive!")
-"""     
-class AtlanticBlueMarlin(Birds):
-    def __init__(self) -> None:
-        pass
-    def act(self):
-        print("\nthe Atlantic Blue Marlin is eating small fishes!")
-        print("\GLUB! GLUB! GLUB!")
 
-class Garfish(Birds):
+
+class Parrot(Birds):
     def __init__(self) -> None:
         pass
+
     def act(self):
-        print("\nthe Garfish is breathing in the water!")
-        print("\GULP! GULP! GULP!")
-"""
-#Child Class Reptiles
+        print("\nthe Parrot is speaking!")
+        print("\nWITWIW! WITWIW! WITWIW!")
+
+
+class Dove(Birds):
+    def __init__(self) -> None:
+        pass
+
+    def act(self):
+        print("\nthe Dove is flying!")
+        print("\nKROO! KROO! KROO!")
+
+
+# Child Class Reptiles
 class Reptiles(Animal):
     def __init__(self) -> None:
         pass
+
     def show(self):
         print("""
               \tReptiles:
-              
+
                 A vertebrate animal of a class that includes snakes, 
                 lizards, crocodiles, turtles, and tortoises. They are 
                 distinguished by having a dry scaly skin and typically 
                 laying soft-shelled eggs on land.
               """)
+
     def alive(self):
         print("\nThis animal is alive!")
 
-"""
-class AtlanticBlueMarlin(Birds):
-    def __init__(self) -> None:
-        pass
-    def act(self):
-        print("\nthe Atlantic Blue Marlin is eating small fishes!")
-        print("\GLUB! GLUB! GLUB!")
 
-class Garfish(Birds):
+class Snake(Reptiles):
     def __init__(self) -> None:
         pass
+
     def act(self):
-        print("\nthe Garfish is breathing in the water!")
-        print("\GULP! GULP! GULP!")
-"""
+        print("\nthe Snake is eating an egg!")
+        print("\nHISS! HISS! HISS!")
+
+
+class Crocodile(Reptiles):
+    def __init__(self) -> None:
+        pass
+
+    def act(self):
+        print("\nthe Crocodile is jaw-eating the deer!")
+        print("\nARGH! ARGH! ARGH!")
+
+def Choices(animalChoice):
+    print("\n****************************************")
+    print("Choices:")
+    if animalChoice == "mammal":
+        print("\tDog")
+        print("\tCat")
+    elif animalChoice == "amphibian":
+        print("\tFrog")
+        print("\tSalamander")
+    elif animalChoice == "fish":
+        print("\tAtlantic Blue Marlin")
+        print("\tGarfish")
+    elif animalChoice == "bird":
+        print("\tParrot")
+        print("\tDove")
+    elif animalChoice == "reptile":
+        print("\tSnake")
+        print("\tCrocodile")
+    print("\n****************************************")
+
+def Options(animal):
+    while True:
+        Choices(animal)
+        user_input = input("\nWhat {} do you want to know? ".format(animal))
+        if user_input == "":
+            print("\nPlease enter something!")
+        elif user_input == "dog":
+            dog.alive()
+            dog.act()
+            break
+        elif user_input == "cat":
+            cat.alive()
+            cat.act()
+            break
+        elif user_input == "frog":
+            frog.alive()
+            frog.act()
+            break
+        elif user_input == "salamander":
+            salamander.alive()
+            salamander.act()
+            break
+        elif user_input == "atlantic blue marlin":
+            atlanticblueMarlin.alive()
+            atlanticblueMarlin.act()
+            break
+        elif user_input == "garfish":
+            garfish.alive()
+            garfish.act()
+            break
+        elif user_input == "parrot":
+            parrot.alive()
+            parrot.act()
+            break
+        elif user_input == "dove":
+            dove.alive()
+            dove.act()
+            break
+        elif user_input == "snake":
+            snake.alive()
+            snake.act()
+            break
+        elif user_input == "crocodile":
+            crocodile.alive()
+            crocodile.act()
+            break
+        else:
+            print("\nPlease enter a valid choice!")
+
 showAnimals = Animal()
 
 mammal = Mammal()
@@ -175,56 +280,55 @@ cat = Cats()
 
 amphibian = Amphibian()
 frog = Frogs()
-snake = Snakes()
+salamander = Salamander()
 
 fish = Fish()
 atlanticblueMarlin = AtlanticBlueMarlin()
 garfish = Garfish()
 
-birds = Birds()
+bird = Birds()
+parrot = Parrot()
+dove = Dove()
 
-reptiles = Reptiles()
-
+reptile = Reptiles()
+snake = Snake()
+crocodile = Crocodile()
 
 while True:
     showAnimals.printAnimals()
     user_input = input("\nWhat animal do you want to know? ")
     if user_input == "":
+        clear_screen()
         print("\nPlease enter something!")
-    elif user_input == "1":
+    elif user_input == "mammal":
+        clear_screen()
         showAnimals.polyAnimal()
         mammal.show()
-        dog.alive()
-        dog.act()
-        cat.alive()
-        cat.act()
-        print("\nYes it works!")
-    elif user_input == "2":
+        Options(user_input)
+    elif user_input == "amphibian":
+        clear_screen()
         showAnimals.polyAnimal()
         amphibian.show()
-        frog.alive()
-        frog.act()
-        snake.alive()
-        snake.act()
-        print("\nYes it works!")
-    elif user_input == "3":
+        Options(user_input)
+    elif user_input == "fish":
+        clear_screen()
         showAnimals.polyAnimal()
         fish.show()
-        atlanticblueMarlin.alive()
-        atlanticblueMarlin.act()
-        garfish.alive()
-        garfish.act()
-        print("\nYes it works!")
-    elif user_input == "4":
+        Options(user_input)
+    elif user_input == "bird":
+        clear_screen()
         showAnimals.polyAnimal()
-        birds.show()
-        print("\nYes it works!")
-    elif user_input == "5":
+        bird.show()
+        Options(user_input)
+    elif user_input == "reptile":
+        clear_screen()
         showAnimals.polyAnimal()
-        reptiles.show()
-        print("\nYes it works!")
-    elif user_input == "6":
+        reptile.show()
+        Options(user_input)
+    elif user_input == "exit":
+        clear_screen()
         print("\n\nThanks Bye!")
         break
     else:
         print("\nPlease enter a valid choice!")
+
